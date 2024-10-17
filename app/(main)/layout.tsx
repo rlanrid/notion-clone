85d1e04171525a3b84eb5d59a3841361216822c0
WJ <rladnrider77@gmail.com>
2024-10-17 21:34:15 +0900

    1017

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -3,6 +3,7 @@
 import { useConvexAuth } from "convex/react";
 import { Spinner } from "@/components/spinner";
 import { redirect } from "next/navigation";
+import { Navigation } from "./_components/navigation";
 
 const MainLayout = ({
   children
@@ -25,6 +26,7 @@ const MainLayout = ({
 
   return (
     <div className="h-full flex dark:bg-[#1F1F1F]">
+      <Navigation />
       <main className="flex-1 h-full overflow-y-auto">
         {children}
       </main>
