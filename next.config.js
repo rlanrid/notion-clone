d32488758210205e92bf88ab0f9bbc5d926cd09e
WJ <rladnrider77@gmail.com>
2024-10-28 16:32:52 +0900

    1028

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: false,
+
   images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "files.edgestore.dev",
+        port: "",
+        pathname: "/**"
+      }
+    ],
     domains: [
       "files.edgestore.dev"
     ]
